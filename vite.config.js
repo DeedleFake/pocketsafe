9c1d98880148b4dbe974348e7971676c36df6ca6
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-07 20:19:51 -0400

    fix a number of configuration problems

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
-  server: {
-    proxy: {
-      '/api': 'http://[::1]:8090',
-    },
-  },
+	plugins: [sveltekit()],
+	server: {
+		proxy: {
+			'/api': 'http://[::1]:8090',
+		},
+	},
 })
