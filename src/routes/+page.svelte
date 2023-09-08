61efa2d8f9f23bdd94132bc0a44b350c07c34f34
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-07 20:04:21 -0400

    Initial Commit.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  let num = 0
+</script>
+
+<button
+  class="px-4 py-2 text-white bg-sky-500 rounded-full hover:bg-sky-700 active:bg-sky-600"
+  on:click={() => {
+    num++
+  }}
+>
+  Clicked: {num}
+</button>
