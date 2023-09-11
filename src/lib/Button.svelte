c025abbb798a1a84c1f89b16daaf45aa91150bb5
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-11 13:57:43 -0400

    change the button colors and add some more

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,3 +1,10 @@
-<button class="btn-primary" {...$$restProps}>
+<script>
+	export let secondary = false
+
+	$: style = 'btn ' + (secondary ? 'btn-secondary' : 'btn-primary')
+	$: extraStyle = $$props['class'] ?? ''
+</script>
+
+<button class="{style} {extraStyle}" {...$$restProps}>
 	<slot />
 </button>
--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,12 +1,14 @@
 <script>
 	export let href = '#'
 	export let button = false
+	export let secondary = false
 
 	$: style = button
-		? 'btn-primary'
-		: 'text-sky-500 hover:text-sky-700 active:text-sky-600'
+		? 'btn ' + (secondary ? 'btn-secondary' : 'btn-primary')
+		: 'text-cyan-500 hover:text-cyan-700 active:text-cyan-600'
+	$: extraStyle = $$props['class'] ?? ''
 </script>
 
-<a class={style} {href} {...$$restProps} on:click>
+<a class="{style} {extraStyle}" {href} {...$$restProps} on:click>
 	<slot />
 </a>
