cbd259a62e4e51b8ae1706f59d3f0543f296a044
DeedleFake <deedlefake@users.noreply.github.com>
2023-09-12 11:58:37 -0400

    scan `static` for Tailwind classes, too

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte}'],
+	content: ['./{src,static}/**/*.{html,js,svelte}'],
 	theme: {
 		extend: {},
 	},
