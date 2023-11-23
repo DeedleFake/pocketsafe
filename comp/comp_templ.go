83bb25499b87d6ca6004790608402c9608e846f1
DeedleFake <deedlefake@users.noreply.github.com>
2023-11-23 11:41:23 -0500

    comp: rename `value` to `Value`

--- comp/comp_templ.go
+++ comp/comp_templ.go
@@ -125,7 +125,7 @@ func Pager(p PagerParams) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = value("page", 1).Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = Value("page", 1).Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -205,7 +205,7 @@ func Pager(p PagerParams) templ.Component {
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
-				templ_7745c5c3_Err = value("page", n).Render(ctx, templ_7745c5c3_Buffer)
+				templ_7745c5c3_Err = Value("page", n).Render(ctx, templ_7745c5c3_Buffer)
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
@@ -271,7 +271,7 @@ func Pager(p PagerParams) templ.Component {
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
-			templ_7745c5c3_Err = value("page", p.Pages).Render(ctx, templ_7745c5c3_Buffer)
+			templ_7745c5c3_Err = Value("page", p.Pages).Render(ctx, templ_7745c5c3_Buffer)
 			if templ_7745c5c3_Err != nil {
 				return templ_7745c5c3_Err
 			}
@@ -465,7 +465,7 @@ func Nav(p NavParams) templ.Component {
 	})
 }
 
-func value(name string, value any) templ.Component {
+func Value(name string, value any) templ.Component {
 	return templ.ComponentFunc(func(ctx context.Context, templ_7745c5c3_W io.Writer) (templ_7745c5c3_Err error) {
 		templ_7745c5c3_Buffer, templ_7745c5c3_IsBuffer := templ_7745c5c3_W.(*bytes.Buffer)
 		if !templ_7745c5c3_IsBuffer {
